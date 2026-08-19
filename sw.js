c82dd15fa271da329901c94249127cfc3d475f64
kkb1337 <kkb1337228@gmail.com>
2026-08-19 19:40:04 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ftracker-v2';
+const CACHE = 'ftracker-v4';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', event => {
