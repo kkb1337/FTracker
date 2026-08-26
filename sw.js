cd510194572890461a0b9060b09021c2dbc53a87
kkb1337 <kkb1337228@gmail.com>
2026-08-26 17:36:30 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_VERSION = 'ftracker-v129';
+const CACHE_NAME = CACHE_VERSION;
+const APP_SHELL = ['./','./index.html','./manifest.json','./icon-192.png'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(()=>self.skipWaiting()));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(()=>self.clients.claim()));
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)).catch(()=>{});
+      return response;
+    }).catch(()=>cached))
+  );
+});
