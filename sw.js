2b956a0914bf543a771aef2a049e0894eac9f0d9
kkb1337 <kkb1337228@gmail.com>
2026-08-27 17:42:21 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'ftracker-v1.0.5-r1';
+const CACHE_VERSION = 'ftracker-v1.0.5-r2';
 const CACHE_NAME = CACHE_VERSION;
 const APP_SHELL = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 const STATIC_DESTINATIONS = new Set(['style','script','font','image','manifest']);
